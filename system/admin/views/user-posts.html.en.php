12098c3e3b4142a2d83982cbf192ce1c56763dbd
feedoo <feedoobox@gmail.com>
2014-05-08 15:16:47 +0200

    add english version, and fix a mistake..

--- system/admin/views/user-posts.html.en.php
+++ system/admin/views/user-posts.html.en.php
@@ -1,7 +1,7 @@
 <h2 class="post-index"><?php echo $heading?></h2>
 <?php if(!empty($posts)) {?>
 <table class="post-list">
-	<tr class="head"><th>Titre</th><th>Publié le</th><th>Tag(s)</th><th>Opérations</th></tr>
+	<tr class="head"><th>Title</th><th>Published</th><th>Tag</th><th>Operations</th></tr>
 	<?php $i = 0; $len = count($posts);?>
 	<?php foreach($posts as $p):?>
 		<?php
@@ -20,18 +20,18 @@
 		<td><a target="_blank" href="<?php echo $p->url ?>"><?php echo $p->title ?></a></td>
 		<td><?php echo date('d F Y', $p->date) ?></td>
 		<td><?php echo $p->tag ?></td>
-		<td><a href="<?php echo $p->url ?>/edit?destination=admin/mine">Éditer</a> <a href="<?php echo $p->url ?>/delete?destination=admin/mine">Supprimer</a></td>
+		<td><a href="<?php echo $p->url ?>/edit?destination=admin/mine">Edit</a> <a href="<?php echo $p->url ?>/delete?destination=admin/mine">Delete</a></td>
 	</tr>
 	<?php endforeach;?>
 </table>
 <?php if (!empty($pagination['prev']) || !empty($pagination['next'])):?>
 	<div class="pager">
 		<?php if (!empty($pagination['prev'])):?>
-			<span><a href="?page=<?php echo $page-1?>" class="pagination-arrow newer" rel="prev">Plus récent</a></span>
+			<span><a href="?page=<?php echo $page-1?>" class="pagination-arrow newer" rel="prev">Newer</a></span>
 		<?php endif;?>
 		<?php if (!empty($pagination['next'])):?>
-			<span><a href="?page=<?php echo $page+1?>" class="pagination-arrow older" rel="next">Plus ancien</a></span>
+			<span><a href="?page=<?php echo $page+1?>" class="pagination-arrow older" rel="next">Older</a></span>
 		<?php endif;?>
 	</div>
 <?php endif;?>
-<?php } else { echo 'Aucun billet trouvé !'; }?>
+<?php } else { echo 'No posts found!'; }?>
